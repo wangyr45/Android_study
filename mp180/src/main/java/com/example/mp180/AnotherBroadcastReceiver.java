9be04e14fb1f22b34eeffb608c0556aec4f9e41c
wangyr45 <wangyuehanw1y2r3@163.com>
2018-11-25 11:28:43 +0800

    commit10

---
+++ mp180/src/main/java/com/example/mp180/AnotherBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.example.mp180;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by 璞毓 on 2018/10/31.
+ */
+
+public class AnotherBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"received in AnotherBroadcastReceiver",Toast.LENGTH_SHORT).show();
+    }
+}
